85b512f42e0ea327ce444df0e56b263c62013cae
malathipraka <malathipriya.p@gmail.com>
2022-10-31 07:58:11 +0530

    prakashgit

---
+++ src/org/Test/EmployeeDetails.java
@@ -0,0 +1,24 @@
+package org.Test;
+
+public class EmployeeDetails {
+	private void empname() {
+		 System.out.println("Anand");
+			}
+			private void empmobile() {
+		System.out.println("siva");
+			}
+		private void empid() {
+		System.out.println("employee id :8072666170");
+		}
+			public static void main(String[] args) {
+				EmployeeDetails E=new EmployeeDetails();
+				E.empmobile();
+				E.empname();
+				E.empid();
+			}
+
+	
+
+	}
+
+
